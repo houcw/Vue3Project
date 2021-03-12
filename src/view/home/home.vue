d0245a62e67d2d447a95310f4985c9fc0fcb2fc1
侯程伟 <h13720613310@163.com>
2021-03-12 11:26:12 +0800

    first commit

---
+++ src/view/home/home.vue
@@ -0,0 +1,28 @@
+
+// 首页
+<template>
+    <div class="homePage">
+        <div class="header">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.homePage{
+    width: 100%;
+    height: 100%;
+    .header{
+        height: 120px;
+        background: red;
+    }
+
+}
+
+</style>
---
+++ src/view/news/news.vue
@@ -0,0 +1,14 @@
+// 新闻列表
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
